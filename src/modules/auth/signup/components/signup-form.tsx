8e2d2b53341f5cd40bb31c3937c52642515e294c
zubeyranwar <zubeyranwar@gmail.com>
2026-08-14 16:59:33 +0300

    chore: test deployment

--- src/modules/auth/signup/components/signup-form.tsx
+++ src/modules/auth/signup/components/signup-form.tsx
@@ -59,7 +59,7 @@ export function SignupForm({
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Create your account</h1>
                 <p className="text-muted-foreground text-sm text-balance">
-                  Enter your details below to create your account
+                  Enter your details below to create your account.
                 </p>
               </div>
               <Field>
